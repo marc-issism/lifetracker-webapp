deed83f05787d78d9503547e41cd1efde4cd4c97
Marcus Cua <moojycua@gmail.com>
2025-02-08 17:57:02 -0500

    created Navbar component

---
+++ life-tracker/src/life-tracker-components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+function Navbar() {
+    return(
+        <div className="navbar">
+            <h2>Navbar</h2>
+            <ul>
+                <li><a href="https://youtube.ca">Life</a></li>
+                <li><a href="https://youtube.ca">Life</a></li>
+                <li><a href="https://youtube.ca">Life</a></li>
+                <li><a href="https://youtube.ca">Life</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
--- life-tracker/src/main.jsx
+++ life-tracker/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import LifeTrackerWeekly from './life-tracker-components/LifeTracker'
 import Legend from './life-tracker-components/Legend';
+import Navbar from './life-tracker-components/Navbar';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar/>
     <LifeTrackerWeekly/>
     <Legend/>
   </StrictMode>,
